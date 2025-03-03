1ed92296a328db0f4346a40e31b45ef9c44c3fc7
Santiago Rodriguez <05rmsantiago@gmail.com>
2025-03-03 00:32:26 -0300

    feat: :sparkles: agregar animaciones responsivas para actividades en escritorio y móvil

--- src/scripts/animations.ts
+++ src/scripts/animations.ts
@@ -2,7 +2,13 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
+window.addEventListener('resize', () => {
+  ScrollTrigger.refresh();
+});
+
 export function initAnimations() {
+  const mm = gsap.matchMedia();
+
   setTimeout(() => {
     gsap.from('.fade-in', {
       opacity: 0,
@@ -17,66 +23,6 @@ export function initAnimations() {
   }, 100);
 
   setTimeout(() => {
-    // Pin de la descripción
-    ScrollTrigger.create({
-      trigger: '.act-desc',
-      start: 'top 0rem',
-      end: '+=200%',
-      pin: true,
-      pinSpacing: false, // Asegura que GSAP no agregue espacio raro
-      scrub: true,
-    });
-
-    // Timeline para centrar cada card
-    const timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.timeline-section',
-        start: 'top+=-100px 10rem',
-        end: '+=200%',
-        scrub: true,
-        pin: true,
-        pinSpacing: false,
-        snap: {
-          snapTo: 'labels',
-          duration: { min: 0.2, max: 0.8 },
-          ease: 'power1.inOut',
-        },
-      },
-    });
-
-    // Aparecen centradas una a una
-    timeline
-      .addLabel('card1')
-      .from('.card-1', {
-        x: -200,
-        opacity: 0,
-        duration: 1,
-        onStart: () => setActiveCard(1),
-      })
-      .addLabel('card2')
-      .from('.card-2', {
-        x: 200,
-        opacity: 0,
-        duration: 1,
-        onStart: () => setActiveCard(2),
-      })
-      .addLabel('card3')
-      .from('.card-3', {
-        y: 200,
-        opacity: 0,
-        duration: 1,
-        onStart: () => setActiveCard(3),
-      });
-
-    function setActiveCard(index) {
-      document
-        .querySelectorAll('.timeline-section .card')
-        .forEach((el, idx) => {
-          el.classList.toggle('scale-105', idx + 1 === index);
-          el.classList.toggle('shadow-xl', idx + 1 === index);
-        });
-    }
-
     gsap.from('.fade-sermons', {
       opacity: 0,
       y: 50,
@@ -150,4 +96,135 @@ export function animateMinistries() {
       '-=0.3'
     );
   });
+}
+
+export function animateActivitiesDesktop() {
+  console.log('Desktop');
+  setTimeout(() => {
+    // Pin de la descripción
+    ScrollTrigger.create({
+      trigger: '.act-desc',
+      start: 'top 0rem',
+      end: '+=200%',
+      pin: true,
+      pinSpacing: false, // Asegura que GSAP no agregue espacio raro
+      scrub: true,
+      markers: true,
+    });
+
+    // Timeline para centrar cada card
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.timeline-section',
+        start: 'top+=-100px 10rem',
+        end: '+=200%',
+        scrub: true,
+        pin: true,
+        pinSpacing: false,
+        markers: true,
+        snap: {
+          snapTo: 'labels',
+          duration: { min: 0.2, max: 0.8 },
+          ease: 'power1.inOut',
+        },
+      },
+    });
+
+    // Aparecen centradas una a una
+    timeline
+      .addLabel('card1')
+      .from('.card-1', {
+        x: -200,
+        opacity: 0,
+        duration: 1,
+        onStart: () => setActiveCard(1),
+      })
+      .addLabel('card2')
+      .from('.card-2', {
+        x: 200,
+        opacity: 0,
+        duration: 1,
+        onStart: () => setActiveCard(2),
+      })
+      .addLabel('card3')
+      .from('.card-3', {
+        y: 200,
+        opacity: 0,
+        duration: 1,
+        onStart: () => setActiveCard(3),
+      });
+
+    function setActiveCard(index) {
+      document
+        .querySelectorAll('.timeline-section .card')
+        .forEach((el, idx) => {
+          el.classList.toggle('scale-105', idx + 1 === index);
+          el.classList.toggle('shadow-xl', idx + 1 === index);
+        });
+    }
+  }, 100);
+}
+
+export function animateActivitiesMobile() {
+  console.log('Mobile');
+  setTimeout(() => {
+    ScrollTrigger.create({
+      trigger: '.act-desc',
+      start: 'top -5rem',
+      end: '+=50%',
+      pin: true,
+      pinSpacing: false,
+      scrub: true,
+      markers: true,
+    });
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.timeline-section',
+        start: 'top-=50px 10%',
+        end: '+=30%',
+        scrub: true,
+        pin: true,
+        pinSpacing: false,
+        snap: {
+          snapTo: 'labels',
+          duration: { min: 0.2, max: 0.8 },
+          ease: 'power1.inOut',
+        },
+        markers: true,
+      },
+    });
+
+    timeline
+      .addLabel('card1')
+      .from('.card-1', {
+        x: -200,
+        opacity: 0,
+        duration: 1,
+        onStart: () => setActiveCard(1),
+      })
+      .addLabel('card2')
+      .from('.card-2', {
+        x: 200,
+        opacity: 0,
+        duration: 1,
+        onStart: () => setActiveCard(2),
+      })
+      .addLabel('card3')
+      .from('.card-3', {
+        y: 200,
+        opacity: 0,
+        duration: 1,
+        onStart: () => setActiveCard(3),
+      });
+
+    function setActiveCard(index) {
+      document
+        .querySelectorAll('.timeline-section .card')
+        .forEach((el, idx) => {
+          el.classList.toggle('scale-105', idx + 1 === index);
+          el.classList.toggle('shadow-xl', idx + 1 === index);
+        });
+    }
+  }, 100);
 }
