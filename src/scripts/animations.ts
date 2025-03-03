f383fb26b40fff4d5e56a8a2cf7d5efea951a041
Santiago Rodriguez <05rmsantiago@gmail.com>
2025-03-03 14:31:08 -0300

    feat: :sparkles: agregar animaciones responsivas para tarjetas en diferentes tamaños de pantalla y ajustar estilos en Activities.astro

--- src/scripts/animations.ts
+++ src/scripts/animations.ts
@@ -48,6 +48,322 @@ export function initAnimations() {
       },
     });
   }, 100);
+
+ // Celular (Mobile): hasta 767px
+  mm.add("(max-width: 767px)", () => {
+    setTimeout(() => {
+      ScrollTrigger.create({
+        trigger: '.act-desc',
+        start: 'top -5rem',
+        end: '+=50%',
+        pin: true,
+        pinSpacing: false,
+        scrub: true,
+      });
+  
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.timeline-section',
+          start: 'top-=50px 10%',
+          end: '+=30%',
+          scrub: true,
+          pin: true,
+          pinSpacing: false,
+          snap: {
+            snapTo: 'labels',
+            duration: { min: 0.2, max: 0.8 },
+            ease: 'power1.inOut',
+          },
+  
+        },
+      });
+  
+      timeline
+        .addLabel('card1')
+        .from('.card-1', {
+          x: -200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(1),
+        })
+        .addLabel('card2')
+        .from('.card-2', {
+          x: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(2),
+        })
+        .addLabel('card3')
+        .from('.card-3', {
+          y: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(3),
+        });
+  
+      function setActiveCard(index: number) {
+        document
+          .querySelectorAll('.timeline-section .card')
+          .forEach((el, idx) => {
+            el.classList.toggle('scale-105', idx + 1 === index);
+            el.classList.toggle('shadow-xl', idx + 1 === index);
+          });
+      }
+    }, 100);
+  });
+
+  // Tablet: entre 768px y 1023px
+  mm.add("(min-width: 768px) and (max-width: 1023px)", () => {
+    setTimeout(() => {
+      ScrollTrigger.create({
+        trigger: '.act-desc',
+        start: 'top -5rem',
+        end: '+=50%',
+        pin: true,
+        pinSpacing: false,
+        scrub: true,
+      });
+  
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.timeline-section',
+          start: 'top-=50px 10%',
+          end: '+=30%',
+          scrub: true,
+          pin: true,
+          pinSpacing: false,
+          snap: {
+            snapTo: 'labels',
+            duration: { min: 0.2, max: 0.8 },
+            ease: 'power1.inOut',
+          },
+  
+        },
+      });
+  
+      timeline
+        .addLabel('card1')
+        .from('.card-1', {
+          x: -200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(1),
+        })
+        .addLabel('card2')
+        .from('.card-2', {
+          x: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(2),
+        })
+        .addLabel('card3')
+        .from('.card-3', {
+          y: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(3),
+        });
+  
+      function setActiveCard(index: number) {
+        document
+          .querySelectorAll('.timeline-section .card')
+          .forEach((el, idx) => {
+            el.classList.toggle('scale-105', idx + 1 === index);
+            el.classList.toggle('shadow-xl', idx + 1 === index);
+          });
+      }
+    }, 100);
+  });
+
+  // Desktop: entre 1024px y 1279px
+  mm.add("(min-width: 1024px) and (max-width: 1279px)", () => {
+    setTimeout(() => {
+      ScrollTrigger.create({
+        trigger: '.act-desc',
+        start: 'top 30rem',
+        end: '+=200%',
+        pin: true,
+        pinSpacing: false, 
+        scrub: true,
+      });
+
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.timeline-section',
+          start: 'top+=-100px 10rem',
+          end: '+=75%',
+          scrub: true,
+          pin: true,
+          pinSpacing: false,
+  
+          snap: {
+            snapTo: 'labels',
+            duration: { min: 0.2, max: 0.8 },
+            ease: 'power1.inOut',
+          },
+        },
+      });
+  
+      timeline
+        .addLabel('card1')
+        .from('.card-1', {
+          x: -200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(1),
+        })
+        .addLabel('card2')
+        .from('.card-2', {
+          x: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(2),
+        })
+        .addLabel('card3')
+        .from('.card-3', {
+          y: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(3),
+        });
+  
+      function setActiveCard(index: number) {
+        document
+          .querySelectorAll('.timeline-section .card')
+          .forEach((el, idx) => {
+            el.classList.toggle('scale-105', idx + 1 === index);
+            el.classList.toggle('shadow-xl', idx + 1 === index);
+          });
+      }
+    }, 100);
+  });
+
+  // Desktop XL: entre 1280px y 1535px
+  mm.add("(min-width: 1280px) and (max-width: 1535px)", () => {
+    setTimeout(() => {
+      ScrollTrigger.create({
+        trigger: '.act-desc',
+        start: 'top 0rem',
+        end: '+=200%',
+        pin: true,
+        pinSpacing: false, 
+        scrub: true,
+      });
+
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.timeline-section',
+          start: 'top+=-100px 10rem',
+          end: '+=100%',
+          scrub: true,
+          pin: true,
+          pinSpacing: false,
+  
+          snap: {
+            snapTo: 'labels',
+            duration: { min: 0.2, max: 0.8 },
+            ease: 'power1.inOut',
+          },
+        },
+      });
+  
+      timeline
+        .addLabel('card1')
+        .from('.card-1', {
+          x: -200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(1),
+        })
+        .addLabel('card2')
+        .from('.card-2', {
+          x: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(2),
+        })
+        .addLabel('card3')
+        .from('.card-3', {
+          y: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(3),
+        });
+  
+      function setActiveCard(index: number) {
+        document
+          .querySelectorAll('.timeline-section .card')
+          .forEach((el, idx) => {
+            el.classList.toggle('scale-105', idx + 1 === index);
+            el.classList.toggle('shadow-xl', idx + 1 === index);
+          });
+      }
+    }, 100);
+  });
+
+  mm.add("(min-width: 1536px)", () => {
+    setTimeout(() => {
+      ScrollTrigger.create({
+        trigger: '.act-desc',
+        start: 'top 0rem',
+        end: '+=200%',
+        pin: true,
+        pinSpacing: false, // Asegura que GSAP no agregue espacio raro
+        scrub: true,
+      });
+  
+      // Timeline para centrar cada card
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.timeline-section',
+          start: 'top+=-100px 10rem',
+          end: '+=100%',
+          scrub: true,
+          pin: true,
+          pinSpacing: false,
+  
+          snap: {
+            snapTo: 'labels',
+            duration: { min: 0.2, max: 0.8 },
+            ease: 'power1.inOut',
+          },
+        },
+      });
+  
+      // Aparecen centradas una a una
+      timeline
+        .addLabel('card1')
+        .from('.card-1', {
+          x: -200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(1),
+        })
+        .addLabel('card2')
+        .from('.card-2', {
+          x: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(2),
+        })
+        .addLabel('card3')
+        .from('.card-3', {
+          y: 200,
+          opacity: 0,
+          duration: 1,
+          onStart: () => setActiveCard(3),
+        });
+  
+      function setActiveCard(index: number) {
+        document
+          .querySelectorAll('.timeline-section .card')
+          .forEach((el, idx) => {
+            el.classList.toggle('scale-105', idx + 1 === index);
+            el.classList.toggle('shadow-xl', idx + 1 === index);
+          });
+      }
+    }, 100);
+  });
 }
 
 export function animateMinistries() {
@@ -96,133 +412,4 @@ export function animateMinistries() {
       '-=0.3'
     );
   });
-}
-
-export function animateActivitiesDesktop() {
-  console.log('Desktop');
-  setTimeout(() => {
-    // Pin de la descripción
-    ScrollTrigger.create({
-      trigger: '.act-desc',
-      start: 'top 0rem',
-      end: '+=200%',
-      pin: true,
-      pinSpacing: false, // Asegura que GSAP no agregue espacio raro
-      scrub: true,
-    });
-
-    // Timeline para centrar cada card
-    const timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.timeline-section',
-        start: 'top+=-100px 10rem',
-        end: '+=100%',
-        scrub: true,
-        pin: true,
-        pinSpacing: false,
-
-        snap: {
-          snapTo: 'labels',
-          duration: { min: 0.2, max: 0.8 },
-          ease: 'power1.inOut',
-        },
-      },
-    });
-
-    // Aparecen centradas una a una
-    timeline
-      .addLabel('card1')
-      .from('.card-1', {
-        x: -200,
-        opacity: 0,
-        duration: 1,
-        onStart: () => setActiveCard(1),
-      })
-      .addLabel('card2')
-      .from('.card-2', {
-        x: 200,
-        opacity: 0,
-        duration: 1,
-        onStart: () => setActiveCard(2),
-      })
-      .addLabel('card3')
-      .from('.card-3', {
-        y: 200,
-        opacity: 0,
-        duration: 1,
-        onStart: () => setActiveCard(3),
-      });
-
-    function setActiveCard(index: number) {
-      document
-        .querySelectorAll('.timeline-section .card')
-        .forEach((el, idx) => {
-          el.classList.toggle('scale-105', idx + 1 === index);
-          el.classList.toggle('shadow-xl', idx + 1 === index);
-        });
-    }
-  }, 100);
-}
-
-export function animateActivitiesMobile() {
-  console.log('Mobile');
-  setTimeout(() => {
-    ScrollTrigger.create({
-      trigger: '.act-desc',
-      start: 'top -5rem',
-      end: '+=50%',
-      pin: true,
-      pinSpacing: false,
-      scrub: true,
-    });
-
-    const timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.timeline-section',
-        start: 'top-=50px 10%',
-        end: '+=30%',
-        scrub: true,
-        pin: true,
-        pinSpacing: false,
-        snap: {
-          snapTo: 'labels',
-          duration: { min: 0.2, max: 0.8 },
-          ease: 'power1.inOut',
-        },
-
-      },
-    });
-
-    timeline
-      .addLabel('card1')
-      .from('.card-1', {
-        x: -200,
-        opacity: 0,
-        duration: 1,
-        onStart: () => setActiveCard(1),
-      })
-      .addLabel('card2')
-      .from('.card-2', {
-        x: 200,
-        opacity: 0,
-        duration: 1,
-        onStart: () => setActiveCard(2),
-      })
-      .addLabel('card3')
-      .from('.card-3', {
-        y: 200,
-        opacity: 0,
-        duration: 1,
-        onStart: () => setActiveCard(3),
-      });
-
-    function setActiveCard(index: number) {
-      document
-        .querySelectorAll('.timeline-section .card')
-        .forEach((el, idx) => {
-          el.classList.toggle('scale-105', idx + 1 === index);
-          el.classList.toggle('shadow-xl', idx + 1 === index);
-        });
-    }
-  }, 100);
 }
